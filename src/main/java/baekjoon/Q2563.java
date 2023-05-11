30d9b8faf8c6bf9c608650ba287bcffe50f8be8c
dothur <craz1000@naver.com>
2023-05-12 01:40:36 +0900

    fix. boolean 배열 풀이

--- src/main/java/baekjoon/Q2563.java
+++ src/main/java/baekjoon/Q2563.java
@@ -7,8 +7,8 @@ public class Q2563 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int[][] arr = new int[100][100];
+        // boolean 배열로 풀기 , [100][100] 으로 했을 때 런타임 에러
+        boolean[][] arr = new boolean[101][101];
         int sizeResult = 0;
         int paperCount = Integer.parseInt(br.readLine());
         for (int i = 0; i < paperCount; i++) {
@@ -17,8 +17,8 @@ public class Q2563 {
             int y = Integer.parseInt(st.nextToken());
             for (int j = x; j < x + 10; j++) {
                 for (int k = y; k < y + 10; k++) {
-                    if (arr[j][k] == 0){
-                        arr[j][k] = 1;
+                    if (!arr[j][k]){
+                        arr[j][k] = true;
                         sizeResult++;
                     }
                 }
