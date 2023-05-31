9ceb69e34a43c00ab1878f3f53bd486f21216777
dothur <craz1000@naver.com>
2023-06-01 00:18:42 +0900

    fix. 최대 힙 - isEmpty() => size()

--- src/main/java/baekjoon/Q11279.java
+++ src/main/java/baekjoon/Q11279.java
@@ -16,7 +16,7 @@ public class Q11279 {
         for (int i = 0; i < num; i++) {
             int x = Integer.parseInt(br.readLine());
             if (x == 0){
-                if (maxHeap.isEmpty()) bw.write("0\n");
+                if (maxHeap.size() == 0) bw.write("0\n");
                 else bw.write(maxHeap.poll() + "\n");
             } else {
                 maxHeap.offer(x);
