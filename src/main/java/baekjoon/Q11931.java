c5a17b4a0f1499af15a50c3a87c6ebbf6a7d99c7
dothur <craz1000@naver.com>
2023-06-01 21:46:46 +0900

    feat. Array 풀이 주석 추가

--- src/main/java/baekjoon/Q11931.java
+++ src/main/java/baekjoon/Q11931.java
@@ -24,3 +24,27 @@ public class Q11931 {
         bw.close();
     }
 }
+/*
+* import java.io.*;
+import java.util.Arrays;
+
+public class Q11931 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int testNum = Integer.parseInt(br.readLine());
+        int[] arr = new int[testNum];
+        for (int i = 0; i < testNum; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+        for (int i = arr.length - 1; i >= 0; i--) {
+            bw.write(arr[i] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+}
+}*/
